1f6844d0d4007bbb90745f80902b6c2f2c50a899
nakyau <nakyau@gmail.com>
2013-01-17 19:42:41 +0900

    p10,p11

--- p0010.hs
+++ p0010.hs
@@ -4,10 +4,13 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 -}
 
-{- Sieve of Eratosthenes -}
-sieve :: [Int] -> [Int]
-sieve (x:xs) = x : sieve [p | p <- xs, mod p x /= 0]
+{- prime number generator / h99 No.31 Solution -}
+isPrime :: (Integral a) => a -> Bool
+isPrime n | n < 4 = n /= 1 
+isPrime n = all ((/=0) . mod n) $ takeWhile (<= m) candidates 
+        where candidates = (2:3:[x + i | x <- [6,12..], i <- [-1,1]])
+              m = floor . sqrt $ fromIntegral n
 
 {- it's too slow -}
 p0010 :: Int
-p0010 = sum $ takeWhile (<= 2000000) $ 2:sieve [3,5..]
+p0010 = sum $ takeWhile (<= 2000000) $ filter isPrime [1..]
